be70ed17fa236e3b242fadb90d13c9361eb882d2
nempati <praveenempati@gmail.com>
2020-02-29 15:18:30 +0530

    changes

--- java/welcome.java
+++ java/welcome.java
@@ -2,7 +2,7 @@ class welcome
 {
 	public static void main(String[] args) 
 	{
-		// prints Hello World! on console
+		// prints content in sys.ot.printin! on console
 		System.out.println("Welcome here this is the test page for seeing!"); 
 	}
 }
